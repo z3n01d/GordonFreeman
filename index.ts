0bf4b6a0fdd15f8aa1667172aa67d10a8962e96f
RealJace <jacemorphing.roblox@gmail.com>
2023-04-13 17:36:59 +0200

    Some stuff

--- index.ts
+++ index.ts
@@ -4,6 +4,13 @@ import * as Eris from "eris";
 import express from "express";
 import fs from "fs";
 import path from "path";
+import sqlite3, { Database } from "sqlite3";
+import { open } from "sqlite";
+
+const database = await open({
+    filename: "main.db",
+    driver: sqlite3.Database
+});
 
 const app = express();
 const port = 3000;
