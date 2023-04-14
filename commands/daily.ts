ce769fafc4b799859a1db9ce77b279592f54357a
RealJace <jacemorphing.roblox@gmail.com>
2023-04-14 21:30:01 +0200

    Update daily.ts

--- commands/daily.ts
+++ commands/daily.ts
@@ -9,7 +9,7 @@ export async function execute(interaction: Eris.CommandInteraction) {
     const userData = getUserData(interaction.member.id);
     if (86400000 - (Date.now() - userData.dailyTime) > 0) {
         const timeLeft = 86400000 - (Date.now() - userData.dailyTime);
-        return interaction.createMessage(`You have already claimed your daily award, try again later after ${(timeLeft / 3600000) % 24}`);
+        return interaction.createMessage(`You have already claimed your daily award, try again later after ${Math.round((timeLeft / 3600000) % 24)}`);
     } else {
         const reward = randomRange(10,500);
         userData.money += reward;
