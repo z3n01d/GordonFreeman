f12b4aa630d62728ef34cdc47082898f64bf5a5e
RealJace <jacemorphing.roblox@gmail.com>
2023-04-15 16:23:45 +0200

    Update search.ts

--- commands/search.ts
+++ commands/search.ts
@@ -1,21 +1,51 @@
 import * as Eris from "eris";
 
+const places = {
+    "citadel": "Citadel",
+    "kleiner": "Kleiner's lab",
+    "novaprospekt": "Nova Prospekt",
+    "train": "Train station",
+    "whiteforest": "White Forest"
+}
+
 export const name: string = "search";
 export const description: string = "Search places in City-17 and maybe you will find something.";
 export const options = [];
 export async function execute(interaction: Eris.Interaction) {
     if (interaction instanceof Eris.CommandInteraction) {
+        var placesClone = Object.keys(places);
+        var place1 = placesClone[Math.floor(Math.random() * placesClone.length)];
+        placesClone.splice(placesClone.indexOf(place1),1);
+        var place2 = placesClone[Math.floor(Math.random() * placesClone.length)];
+        placesClone.splice(placesClone.indexOf(place2),1);
+        var place3 = placesClone[Math.floor(Math.random() * placesClone.length)];
         return interaction.createMessage({
-            content: "This is a message with a button.",
+            embeds: [
+                {
+                    title: "Where do you want to search?"
+                }
+            ],
             components: [
                 {
                     type: Eris.Constants.ComponentTypes.ACTION_ROW,
                     components: [
                         {
                             type: Eris.Constants.ComponentTypes.BUTTON,
-                            label: "clic me now",
+                            label: places[place1],
                             style: Eris.Constants.ButtonStyles.PRIMARY,
-                            custom_id: "yes"
+                            custom_id: place1
+                        },
+                        {
+                            type: Eris.Constants.ComponentTypes.BUTTON,
+                            label: places[place2],
+                            style: Eris.Constants.ButtonStyles.PRIMARY,
+                            custom_id: place2
+                        },
+                        {
+                            type: Eris.Constants.ComponentTypes.BUTTON,
+                            label: places[place3],
+                            style: Eris.Constants.ButtonStyles.PRIMARY,
+                            custom_id: place3
                         }
                     ]
                 }
