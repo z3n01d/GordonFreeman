f9bc6585d434957b95d59ab67093d1425b3fcd9d
RealJace <jacemorphing.roblox@gmail.com>
2023-04-15 15:53:11 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -103,12 +103,10 @@ client.on("interactionCreate",async (interaction: Eris.Interaction) => {
         return command.execute(interaction);
     }
     if (interaction instanceof Eris.ComponentInteraction) {
-        console.log(interaction.message.interaction);
         if (
-            typeof(interaction.message.interaction) === "undefined" ||
-            !(interaction.message.interaction instanceof Eris.CommandInteraction)
+            typeof(interaction.message.interaction) === "undefined"
         ) return;
-        const command = commands[interaction.message.interaction.data.name];
+        const command = commands[interaction.message.interaction.name];
         return command.execute(interaction);
     }
 })
