ec75ef260392cca8a3b7718852a576f9fc00ce4f
RealJace <jacemorphing.roblox@gmail.com>
2023-04-16 21:56:12 +0200

    Added guild settings

---
+++ commands/settings.ts
@@ -0,0 +1,57 @@
+import * as Eris from "eris";
+import { client, getGuildData, setGuildData } from "..";
+
+export const name: string = "settings";
+export const description: string = "Commands related to settings";
+export const options = [
+    {
+        type: Eris.Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+        name: "set",
+        description: "Set a setting to a value.",
+        options: [
+            {
+                type: Eris.Constants.ApplicationCommandOptionTypes.STRING,
+                name: "setting",
+                description: "What setting do you want to edit?",
+                required: true,
+                choices: [
+                    {
+                        name: "Leveling channel id",
+                        value: "levelChannelId"
+                    }
+                ]
+            },
+            {
+                type: Eris.Constants.ApplicationCommandOptionTypes.STRING,
+                name: "newvalue",
+                description: "New value of a setting",
+                required: false,
+            }
+        ]
+    },
+];
+export async function execute(interaction: Eris.Interaction) {
+    if (!(interaction instanceof Eris.CommandInteraction)) return;
+    if (typeof(interaction.member) === "undefined") return;
+    if (typeof(interaction.guildID) === "undefined") return;
+    if (typeof(interaction.data.options) === "undefined") return;
+    if (!interaction.member.permissions.json.administrator) return interaction.createMessage({
+        content: "You have to have Administrator permissions to use this command.",
+        flags: 64
+    });
+
+    if (interaction.data.options[0].name == "set") {
+        const setting = interaction.data.options[0]["options"][0].value;
+        const newValue = interaction.data.options[0]["options"][1].value;
+        try {
+            var guildData = getGuildData(interaction.guildID);
+            if (guildData != null) {
+                guildData.settings[setting] = newValue;
+            }
+        } catch (error) {
+            console.log(error);
+            return interaction.createMessage(`Failed to update setting ${setting} to ${newValue}`);
+        }
+        return interaction.createMessage(`Successfully updated setting ${setting} to ${newValue}`);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { Channel } from "diagnostics_channel";
 import dotenv from "dotenv";
 dotenv.config();
 import * as Eris from "eris";
@@ -12,6 +13,14 @@ type UserData = {
     searchTime: number;
     level: number;
     xp: number;
+}
+
+type GuildSettings = {
+    levelChannelId: string | null
+}
+
+type GuildData = {
+    settings: GuildSettings;
 }
 
 const app = Express();
@@ -38,6 +47,19 @@ export function setUserData(userId: string,newUserData) {
     const data = JSON.parse(rawData);
     data[userId] = newUserData || {};
     fs.writeFileSync("userdata.json",JSON.stringify(data,null,4));
+}
+
+export function getGuildData(guildId: string): GuildData | null {
+    const rawData = fs.readFileSync("guilddata.json","utf-8");
+    const data = JSON.parse(rawData);
+    return data[guildId] || null;
+}
+
+export function setGuildData(guildId: string,newGuildData) {
+    const rawData = fs.readFileSync("guilddata.json","utf-8");
+    const data = JSON.parse(rawData);
+    data[guildId] = newGuildData || {};
+    fs.writeFileSync("guilddata.json",JSON.stringify(data,null,4));
 }
 
 function setupUsersData(userId: string) {
@@ -67,6 +89,24 @@ function setupUsersData(userId: string) {
             userData.xp = 0;
         }
         setUserData(userId,userData);
+    }
+}
+
+function setupGuildsData(guildId: string) {
+    var guildData = getGuildData(guildId);
+    if (guildData == null) {
+        setGuildData(guildId,{
+            settings: {
+                levelChannelId: null
+            }
+        });
+    } else {
+        if (typeof(guildData.settings) === "undefined") {
+            guildData.settings = {
+                levelChannelId: null
+            };
+        }
+        setGuildData(guildId,guildData);
     }
 }
 
@@ -107,23 +147,34 @@ client.on("error",(err) => {
     console.error(err);
 })
 
-client.on("messageCreate",(message) => {
+client.on("messageCreate",async (message) => {
     if (message.author.bot) return;
+    setupGuildsData(message.guildID);
     setupUsersData(message.author.id);
     var userData = getUserData(message.author.id);
     userData.xp += 1;
     if (userData.xp >= userData.level * 10) {
         userData.level += 1;
-        if (message.channel instanceof Eris.TextChannel) {
-            message.channel.createMessage({
-                content: `${message.author.mention} has reached **level ${userData.level.toString()}**!`,
-                messageReference: {
-                    channelID: message.channel.id,
-                    failIfNotExists: false,
-                    guildID: message.guildID,
-                    messageID: message.id
-                }
-            });
+        let messageData = {
+            content: `${message.author.mention} has reached **level ${userData.level.toString()}**!`,
+        }
+        const guildData = getGuildData(message.guildID);
+        var channel = null;
+
+        if (guildData != null && guildData.settings.levelChannelId != null) {
+            try {
+                channel = await client.getRESTChannel(guildData.settings.levelChannelId)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if (channel && channel instanceof Eris.TextChannel) {
+            channel.createMessage(messageData);
+        } else {
+            if (message.channel instanceof Eris.TextChannel) {
+                message.channel.createMessage(messageData);
+            }
         }
         userData.xp = 0;
     }
@@ -133,6 +184,7 @@ client.on("messageCreate",(message) => {
 client.on("interactionCreate",async (interaction: Eris.Interaction) => {
     if (interaction instanceof Eris.CommandInteraction) {
         if (!interaction.member) return interaction.createMessage("You can only use this command in a guild!");
+        setupGuildsData(interaction.guildID);
         setupUsersData(interaction.member.id);
         const command = commands[interaction.data.name];
         await interaction.defer();
