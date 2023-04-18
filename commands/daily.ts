2a745dba090cef1b27aff74e761e4966931b702a
RealJace <jacemorphing.roblox@gmail.com>
2023-04-18 21:55:49 +0200

    Voting xd

--- commands/daily.ts
+++ commands/daily.ts
@@ -9,12 +9,27 @@ export async function execute(interaction: Eris.Interaction) {
     var userData = getUserData(interaction.member.id);
     if (86400000 - (Date.now() - userData.dailyTime) > 0) {
         const timeLeft = 86400000 - (Date.now() - userData.dailyTime);
-        return interaction.createMessage(`You have already claimed your daily award, try again later after **${Math.round((timeLeft / 3600000) % 24)} hours**`);
-    } else {
+        return interaction.createMessage({
+            embeds: [
+                {
+                    title: `You have already claimed your daily award, try again later after **${Math.round((timeLeft / 3600000) % 24)} hours**.`,
+                    description: "Or you can vote for us on [Top.gg](https://top.gg/bot/1095076634144559104/vote) to claim your next award right away!"
+                }
+            ]
+        });
+    } else if ((86400000 - (Date.now() - userData.dailyTime) <= 0) || userData.voted) {
         const reward = randomRange(10,500);
         userData.money += reward;
         userData.dailyTime = Date.now();
+        userData.voted = false;
         setUserData(interaction.member.id,userData);
-        return interaction.createMessage(`You have claimed your daily award of $${reward.toString()}.`);
+        return interaction.createMessage({
+            embeds: [
+                {
+                    title: `You have claimed your daily award of $${reward.toString()}.`,
+                    color: 16755968
+                }
+            ]
+        });
     }
 }
