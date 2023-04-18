8278b862232a706103e8598e3fec0c1711b94ffe
RealJace <jacemorphing.roblox@gmail.com>
2023-04-18 19:03:39 +0200

    Update level.ts

--- commands/level.ts
+++ commands/level.ts
@@ -1,6 +1,6 @@
 import * as Eris from "eris";
 import { client, getUserData } from "..";
-//import canvacord from "canvacord";
+import canvacord from "canvacord";
 
 export const name: string = "level";
 export const description: string = "Check what level you are and how many xp you have.";
@@ -8,7 +8,7 @@ export const options = [];
 export async function execute(interaction: Eris.Interaction) {
     if (!(interaction instanceof Eris.CommandInteraction)) return;
     const userData = getUserData(interaction.member.id);
-    /*
+    
     var rankCard = new canvacord.Rank();
     rankCard.setAvatar(interaction.member.user.avatar || interaction.member.user.defaultAvatar);
     rankCard.setCurrentXP(userData.xp);
@@ -22,14 +22,15 @@ export async function execute(interaction: Eris.Interaction) {
         file: data,
         name: "rankcard.png"
     });
+    /*
+    return interaction.createMessage({
+        embeds: [
+            {
+                title: `${interaction.member.username}'s level status`,
+                description: `Level: ${userData.level.toString()}\nXP: ${userData.xp.toString()}/${(userData.level * 10).toString()}`,
+                color: 16755968
+            }
+        ]
+    });
     */
-   return interaction.createMessage({
-    embeds: [
-        {
-            title: `${interaction.member.username}'s level status`,
-            description: `Level: ${userData.level.toString()}\nXP: ${userData.xp.toString()}/${(userData.level * 10).toString()}`,
-            color: 16755968
-        }
-    ]
-   });
 }
