c07f0e70888fcba5bfffa239e04a2f8526d0c952
RealJace <jacemorphing.roblox@gmail.com>
2023-04-18 19:06:14 +0200

    Update level.ts

--- commands/level.ts
+++ commands/level.ts
@@ -9,28 +9,30 @@ export async function execute(interaction: Eris.Interaction) {
     if (!(interaction instanceof Eris.CommandInteraction)) return;
     const userData = getUserData(interaction.member.id);
     
-    var rankCard = new canvacord.Rank();
-    rankCard.setAvatar(interaction.member.user.avatar || interaction.member.user.defaultAvatar);
-    rankCard.setCurrentXP(userData.xp);
-    rankCard.setRequiredXP(userData.level * 10);
-    rankCard.setProgressBar("#ffad00", "COLOR");
-    rankCard.setUsername(interaction.member.username);
-    rankCard.setDiscriminator(interaction.member.user.discriminator);
-    rankCard.setBackground("IMAGE","https://cdn.discordapp.com/attachments/1096754012302364693/1096778705453461574/server_banner.png");
-    let data = await rankCard.build();
-    return interaction.createMessage("",{
-        file: data,
-        name: "rankcard.png"
-    });
-    /*
-    return interaction.createMessage({
-        embeds: [
-            {
-                title: `${interaction.member.username}'s level status`,
-                description: `Level: ${userData.level.toString()}\nXP: ${userData.xp.toString()}/${(userData.level * 10).toString()}`,
-                color: 16755968
-            }
-        ]
-    });
-    */
+    try {
+        var rankCard = new canvacord.Rank();
+        rankCard.setAvatar(interaction.member.user.avatar || interaction.member.user.defaultAvatar);
+        rankCard.setCurrentXP(userData.xp);
+        rankCard.setRequiredXP(userData.level * 10);
+        rankCard.setProgressBar("#ffad00", "COLOR");
+        rankCard.setUsername(interaction.member.username);
+        rankCard.setDiscriminator(interaction.member.user.discriminator);
+        rankCard.setBackground("IMAGE","https://cdn.discordapp.com/attachments/1096754012302364693/1096778705453461574/server_banner.png");
+        let data = await rankCard.build();
+        return interaction.createMessage("",{
+            file: data,
+            name: "rankcard.png"
+        });
+    } catch (error) {
+        console.log(error);
+        return interaction.createMessage({
+            embeds: [
+                {
+                    title: `${interaction.member.username}'s level status`,
+                    description: `Level: ${userData.level.toString()}\nXP: ${userData.xp.toString()}/${(userData.level * 10).toString()}`,
+                    color: 16755968
+                }
+            ]
+        });
+    }
 }
