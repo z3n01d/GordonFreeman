3ee38272455967759309182b7f43227d59dd1a50
RealJace <jacemorphing.roblox@gmail.com>
2023-04-19 16:17:10 +0200

    Update daily.ts

--- commands/daily.ts
+++ commands/daily.ts
@@ -7,17 +7,7 @@ export const options = [];
 export async function execute(interaction: Eris.Interaction) {
     if (!(interaction instanceof Eris.CommandInteraction)) return;
     var userData = getUserData(interaction.member.id);
-    if (userData.voted === false || (86400000 - (Date.now() - userData.dailyTime) > 0)) {
-        const timeLeft = 86400000 - (Date.now() - userData.dailyTime);
-        return interaction.createMessage({
-            embeds: [
-                {
-                    title: `You have already claimed your daily award, try again later after **${Math.round((timeLeft / 3600000) % 24)} hours**.`,
-                    description: "Or you can vote for us on [Top.gg](https://top.gg/bot/1095076634144559104/vote) to claim your next award right away!"
-                }
-            ]
-        });
-    } else if (userData.voted === true || (86400000 - (Date.now() - userData.dailyTime) <= 0)) {
+    if (userData.voted === true || (86400000 - (Date.now() - userData.dailyTime) <= 0)) {
         const reward = randomRange(10,500);
         userData.money += reward;
         userData.dailyTime = Date.now();
@@ -28,6 +18,16 @@ export async function execute(interaction: Eris.Interaction) {
                 {
                     title: `You have claimed your daily award of $${reward.toString()}.`,
                     color: 16755968
+                }
+            ]
+        });
+    } else {
+        const timeLeft = 86400000 - (Date.now() - userData.dailyTime);
+        return interaction.createMessage({
+            embeds: [
+                {
+                    title: `You have already claimed your daily award, try again later after **${Math.round((timeLeft / 3600000) % 24)} hours**.`,
+                    description: "Or you can vote for us on [Top.gg](https://top.gg/bot/1095076634144559104/vote) to claim your next award right away!"
                 }
             ]
         });
